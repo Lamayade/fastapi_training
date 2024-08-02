d1ee931c4b1c9014f5d0301b3fa1d9384e195fa4
Никита Зивенко <flyingfry@yandex.ru>
2024-08-02 14:33:30 +0300

    Add annotations

--- main.py
+++ main.py
@@ -11,14 +11,29 @@ class EducationLevel(str, Enum):
     HIGHER = 'Высшее образование'
 
 
-@app.get('/{name}')
+@app.get(
+    '/{name}',
+    tags=['common methods'],
+    summary='Общее приветствие',
+    response_description='Полная строка приветствия',
+)
 def greetings(
+    *,
     name: str,
     surname: str,
     age: Optional[int] = None,
     is_staff: bool = False,
     education_level: Optional[EducationLevel] = None,
 ) -> dict[str, str]:
+    """
+    Приветствие пользователя:
+
+    - **name**: имя
+    - **surname**: фамилия
+    - **age**: возраст (опционально)
+    - **is_staff**: является ли пользователь сотрудником
+    - **education_level**: уровень образования (опционально)
+    """
     result = ' '.join([name, surname])
     result = result.title()
     if age is not None:
